cb017859c124fc25b73bedf9d60e4c1849f481bc
void <lvoidiss@gmail.com>
2023-06-29 21:26:45 -0600

    added several changes

--- PCNAS.py
+++ PCNAS.py
@@ -1,6 +1,8 @@
 from tkinter import *
-import tkinter as ui
+from threading import Thread, Event
+import tkinter 
 import os
+import http.server, socketserver
 
 # Colors
 black = "\x1b[0;30m"
@@ -12,59 +14,63 @@ purple = "\x1b[0;35m"
 cyan = "\x1b[0;36m"
 white = "\x1b[0;37m"
 
-try:
-    print(blue+"Starting PC-NAS 1.0")
+Handler = http.server.SimpleHTTPRequestHandler
 
-    def error(errortype):
-        ERROR = ui.Tk()
-        ERROR.title("PC-NAS ERROR")
-        ERROR.geometry("250x80")
-        ERROR.configure(bg="black")
+def init_server(path, port, event: Event):
+    with socketserver.TCPServer((path, int(port)), Handler) as httpd:
+        print("Servidor HTTP en el puerto", port)
+        t3 = Thread(target=httpd.serve_forever)
+        t3.start()
+        while not event.is_set():
+            pass
+        exit()
 
-        Label(ERROR, text="[PC-NAS ERROR]", bg="red", fg="black").pack(fill = ui.X)
-        Label(ERROR, text=" ", bg="black", fg="black").pack(fill = ui.X)
-        Label(ERROR, text="ERROR: "+str(errortype), bg="black", fg="white").pack(fill = ui.X)
+print(blue+"Starting PC-NAS 1.0")
 
-        ERROR.mainloop()
-    
-    UI = ui.Tk()
-    UI.title("PC-NAS 1.0 - by x04000")
-    UI.geometry("520x520")
-    UI.configure(bg="black")
+def on_error(error_type):
+    print("oh no")
+    error = tkinter.Tk()
+    error.title("PC-NAS error")
+    error.geometry("250x80")
+    error.configure(bg="black")
 
-    Label(UI, text="[PC-NAS Configurator]", bg="cyan", fg="black").pack(fill = ui.X)
-    Label(UI, text=" ", bg="black", fg="black").pack(fill = ui.X)
-    Label(UI, text="Select the path of the files you want to share:", bg="black", fg="white").pack(fill = ui.X)
-    pathinput = ui.Entry(UI, width=30, fg="white", bg="black")
-    pathinput.insert(0, os.getcwd())
-    pathinput.pack(fill=ui.X)
-    Label(UI, text="Port:", bg="black", fg="white").pack(fill = ui.X)
-    portinput = ui.Entry(UI, width=30, fg="white", bg="black")
-    portinput.insert(0, "8000")
-    portinput.pack(fill=ui.X)
+    Label(error, text="[PC-NAS error]", bg="red", fg="black").pack(fill = tkinter.X)
+    Label(error, text=" ", bg="black", fg="black").pack(fill = tkinter.X)
+    Label(error, text="error: "+str(error_type), bg="black", fg="white").pack(fill = tkinter.X)
 
-    def start():
-        path = str(pathinput.get())
-        port = str(portinput.get())
-        
-        print("Starting server at: "+path)
-        print("Server port: "+port)
-        try:
-            os.system("python3 -m http.server --directory "+path+" "+port)
-        except KeyboardInterrupt:
-            error("Keyboard Interrupt")
-        except:
-            error("Unknow error")
+    error.mainloop()
 
-    Label(UI, text=" ", bg="black", fg="black").pack(fill = ui.X)
-    Label(UI, text="Warning: If you want to stop the server, do Ctrl+C in the main console", bg="black", fg="white").pack(fill = ui.X)
-    Label(UI, text=" ", bg="black", fg="black").pack(fill = ui.X)
-    
-    ui.Button(UI, text = "Start Server", command = start, fg="white", bg="black").pack(fill = ui.X)
+ui = tkinter.Tk()
+ui.title("PC-NAS 1.0 - by x04000")
+ui.geometry("520x520")
+ui.configure(bg="black")
 
-    UI.mainloop()
+Label(ui, text="[PC-NAS Configurator]", bg="cyan", fg="black").pack(fill = tkinter.X)
+Label(ui, text=" ", bg="black", fg="black").pack(fill = tkinter.X)
+Label(ui, text="Select your host:", bg="black", fg="white").pack(fill = tkinter.X)
+pathinput = tkinter.Entry(ui, width=30, fg="white", bg="black")
+pathinput.insert(0, "localhost")
+pathinput.pack(fill=tkinter.X)
+Label(ui, text="Port:", bg="black", fg="white").pack(fill = tkinter.X)
+portinput = tkinter.Entry(ui, width=30, fg="white", bg="black")
+portinput.insert(0, "8000")
+portinput.pack(fill=tkinter.X)
 
-except KeyboardInterrupt:
-    print(cyan+"["+red+"!"+cyan+"]"+red+"Keyboard Interrupt")
-except:
-    print(cyan+"["+red+"!"+cyan+"]"+red+"Unknow error")
+event = Event()
+path = pathinput.get()
+port = portinput.get()
+t2 = Thread(target=init_server, args=(path, port, event,))
+
+def exit_app():
+    global on_exit
+    ui.destroy()
+    event.set()
+    t2.join()
+    print("Yeah, the following error is supposed to happen")
+    exit()
+
+tkinter.Button(ui, text = "Start Server", command = t2.start, fg="white", bg="black").pack(fill = tkinter.X)
+exit_button = tkinter.Button(ui, text="Exit server", command=exit_app, fg="white", bg="black")
+exit_button.pack()
+
+ui.mainloop()
